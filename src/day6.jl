a54ddd47741eb48466f48436f9cbc5a88e5bdd73
JordiManyer <j.manyer@hotmail.com>
2021-12-13 17:34:56 +0100

    Added days 6 & 7

---
+++ src/day6.jl
@@ -0,0 +1,51 @@
+using DelimitedFiles
+
+mutable struct Lanternfish
+  days:: Int
+  n :: Int
+  groups :: Vector{Int}
+end
+
+function Lanternfish(fish::Vector{Int})
+  n = 0
+  groups = zeros(Int, 9)
+  map(x -> groups[x+1] += 1 , fish)
+  n = sum(groups)
+  return Lanternfish(0,n,groups)
+end
+
+function nextDay!(lf::Lanternfish)
+  permute!(lf.groups,[2,3,4,5,6,7,8,9,1])
+  lf.groups[7] += lf.groups[9]
+  lf.n += lf.groups[9]
+  lf.days += 1
+end
+
+function nextDays!(lf::Lanternfish,nDays::Int) 
+  for i in 1:nDays
+    nextDay!(lf)
+  end
+end
+
+# Read input
+function readInput() :: Vector{Int}
+  data = readdlm("./inputs/day6.txt", ',', Int)
+  return reshape(data,length(data))
+end
+
+println("Problem 1: ")
+fish = readInput()
+lf = Lanternfish(fish)
+nextDays!(lf,80)
+println("Number of days: ", lf.days)
+println("Number of fish: ", lf.n)
+println("Final state: ", lf.groups)
+
+
+println("Problem 2: ")
+fish = readInput()
+lf = Lanternfish(fish)
+nextDays!(lf,256)
+println("Number of days: ", lf.days)
+println("Number of fish: ", lf.n)
+println("Final state: ", lf.groups)
---
+++ src/day7.jl
@@ -0,0 +1,44 @@
+using DelimitedFiles
+
+
+additiveFactorial(n::Int) :: Int = sum(1:n)
+
+distance(pos1::Int, pos2::Int) :: Int = abs(pos1-pos2)
+
+cost(t::Int,positions::Vector{Int}) :: Int = sum(map(p -> distance(t,p), positions))
+
+function cost2(t::Int,positions::Vector{Int}) :: Int 
+  distances = map(p -> distance(t,p), positions)
+  return sum( map( d -> additiveFactorial(d) , distances))
+end
+
+function minimalAlignment(positions::Vector{Int}) :: Tuple{Int,Int}
+  targets = Vector(0:maximum(positions))
+  costs = map(t -> cost(t,positions), targets)
+  i_best = argmin(costs)
+  return (i_best, costs[i_best])
+end
+
+function minimalAlignment2(positions::Vector{Int}) :: Tuple{Int,Int}
+  targets = Vector(0:maximum(positions))
+  costs = map(t -> cost2(t,positions), targets)
+  i_best = argmin(costs)
+  return (i_best, costs[i_best])
+end
+
+# Read input
+function readInput() :: Vector{Int}
+  data = readdlm("./inputs/day7.txt", ',', Int)
+  return reshape(data,length(data))
+end
+
+println("Problem 1: ")
+positions = readInput()
+res = minimalAlignment(positions)
+display(res)
+
+
+println("Problem 2: ")
+positions = readInput()
+res = minimalAlignment2(positions)
+display(res)
